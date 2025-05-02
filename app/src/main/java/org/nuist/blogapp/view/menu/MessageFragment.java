b389e3ebeb4c5de7d193ee09ec319fb52e55311c
yffdxhc <1836947338@qq.com>
2025-05-02 22:53:02 +0800

    [Nuist][BlogApp]消息界面布局微调
    Signed-off-by: yffdxhc <1836947338@qq.com>

--- app/src/main/java/org/nuist/blogapp/view/menu/MessageFragment.java
+++ app/src/main/java/org/nuist/blogapp/view/menu/MessageFragment.java
@@ -3,19 +3,21 @@ package org.nuist.blogapp.view.menu;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 
-import org.nuist.blogapp.MainActivity;
-import org.nuist.blogapp.R;
+import org.nuist.blogapp.ViewModel.UserViewModel;
 import org.nuist.blogapp.databinding.FragmentMessageBinding;
 
 public class MessageFragment extends Fragment{
     private static final String TAG ="MessageFragment";
     private FragmentMessageBinding binding;
+    private UserViewModel userViewModel;
 
     private ViewStub loginStub;
     private ViewStub loggedInStub;
@@ -27,6 +29,7 @@ public class MessageFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
     }
 
     @Override
@@ -40,8 +43,26 @@ public class MessageFragment extends Fragment{
         // 未登录默认布局
         showLoginLayout();
 
+        setListener();
         return binding.getRoot();
     }
+
+    /**
+     * 监听事件
+     */
+    private void setListener(){
+        userViewModel.getToken().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (s != null && !s.isEmpty()){
+                    showLoggedInLayout();
+                }else{
+                    showLoginLayout();
+                }
+            }
+        });
+    }
+
     /**
      * 显示登录前布局
      */
